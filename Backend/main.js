235db5d7e41b6ea911e8a957dd7cba069f685bea
Pratham G Bhat <prathambhat2007@gmail.com>
2026-01-31 21:46:53 +0530

    Fixed main

--- Backend/main.js
+++ Backend/main.js
@@ -1,11 +1,5 @@
 import dotenv from 'dotenv';
-import path from 'path';
-import {fileURLToPath} from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '.env') });
-
+dotenv.config();
 import express from 'express';
 import { google } from 'googleapis';
 import { createGmailFilter, deleteGmailFilter } from './filter_ops.js';
