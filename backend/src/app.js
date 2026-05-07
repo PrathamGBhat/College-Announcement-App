e42be4c320e4dc1960db3483943eac8a86845ee0
Pratham G Bhat <prathambhat2007@gmail.com>
2026-05-07 22:06:27 +0530

    SameSite: none

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,7 +33,7 @@ app.use(session({
   cookie: {
     secure: env.NODE_ENV === 'production',
     httpOnly : true,
-    sameSite : 'lax',
+    sameSite : 'none',
     maxAge: 24 * 60 * 60 * 1000,  // 24 hours
   },
   resave: false,
